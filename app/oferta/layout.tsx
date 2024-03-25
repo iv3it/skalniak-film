7bcfe86463f4b272b6f7e90a41ab30f04fc47b70
Kinga <kinga.sabat@gmail.com>
2024-03-26 00:29:43 +0100

    update metadata

--- app/oferta/layout.tsx
+++ app/oferta/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Oferta Ślubna | Skalniak FILM | Kazimierz Skalniak",
+  description: "Oferta ślubna, zdjęcia, film.",
+};
+
 export default function DashboardLayout({
   children,
 }: {
