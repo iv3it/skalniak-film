98b699dd88947b82872729965e359a25449309c4
Kinga <kinga.sabat@gmail.com>
2024-03-25 01:08:36 +0100

    hero update

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-import { Old_Standard_TT } from '../../fonts';
 import Image from "next/image";
 import styles from './hero.module.css';
 
@@ -15,7 +14,7 @@ const Hero = () => {
           priority
         />
         <div className="flex flex-col justify-between md:items-center md:flex-row-reverse">
-          <h1 className="text-[3.2rem] lg:text-[4rem] font-medium text-left md:text-right">Fotografia <span className={`${Old_Standard_TT.className} italic font-semibold`}>&</span> film</h1>
+          <h1 className="text-[3.2rem] lg:text-[4rem] font-medium text-left md:text-right">Fotografia & film</h1>
           <h2 className="text-[0.95rem] my-4 md:my-0 pr-8 md:max-w-[17.5rem]">Hej, mam na imię Kazimierz. Zajmuję się filmowaniem oraz fotografią. Współpracuję sam lub w duecie z Kamilem, który jest fotografem.</h2>
         </div>
       </div>
