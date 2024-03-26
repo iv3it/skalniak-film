fb904bc38b750023b3c6a842bd65b06a5834821e
Kinga <kinga.sabat@gmail.com>
2024-03-26 23:55:13 +0100

    hero update & added animation

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,10 +1,39 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect, useRef } from 'react';
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import styles from './hero.module.css';
 
+gsap.registerPlugin(useGSAP);
+
 const Hero = () => {
+  const hero = useRef<HTMLElement | any>();
+  const tl = useRef<GSAPTimeline | null>(null);
+
+  useGSAP(() => {
+    tl.current = gsap.timeline().to('.gsap-box',
+    {
+      width: 0,
+      duration: 0.7,
+      stagger: 0.1,
+    })
+  }, {
+    scope: hero,
+  });
+
   return ( 
-    <section className='py-16'>
-      <div className="container mx-auto px-4">
+    <section className='relative flex items-end min-h-screen py-16' ref={hero}>
+      <div className="absolute top-0 left-0 w-full h-full">
+        <div className={styles.boxes}>
+          <div className={`${styles.box} gsap-box`}></div>
+          <div className={`${styles.box} gsap-box`}></div>
+          <div className={`${styles.box} gsap-box`}></div>
+          <div className={`${styles.box} gsap-box`}></div>
+          <div className={`${styles.box} gsap-box`}></div>
+          <div className={`${styles.box} gsap-box`}></div>
+        </div>
         <Image
           src="/hero.jpg"
           alt="background image"
@@ -13,9 +42,11 @@ const Hero = () => {
           height={900}
           priority
         />
+      </div>
+      <div className="container mx-auto px-4">
         <div className="flex flex-col justify-between md:items-center md:flex-row-reverse">
-          <h1 className="text-[3.2rem] lg:text-[4rem] font-medium text-left md:text-right">Fotografia & film</h1>
-          <h2 className="text-[0.95rem] mt-8 md:my-0 pr-8 md:max-w-[17.5rem]">Hej, mam na imię Kazimierz. Zajmuję się filmowaniem oraz fotografią. Współpracuję sam lub w duecie z Kamilem, który jest fotografem.</h2>
+          <h1 className="text-[var(--white)] text-[3.2rem] lg:text-[4rem] font-medium text-left md:text-right">Fotografia & film</h1>
+          <h2 className="text-[var(--white)] text-[0.95rem] mt-8 md:my-0 pr-8 md:max-w-[19.5rem]">Hej, mam na imię Kazimierz. Zajmuję się filmowaniem oraz fotografią ślubną i eventową. Współpracuję sam lub w duecie z Kamilem, który jest fotografem.</h2>
         </div>
       </div>
     </section>
--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation = () => {
         <nav className='hidden lg:flex justify-between items-center py-3 gsap-navigation'>
           <h2 className={`${styles.logo}`}><Link href="/">Kazimierz<br/>Skalniak</Link></h2>
           <ul className={styles.navList}>
-            <li className={styles.navListItem}><Link href="/portfolio" className={styles.navListItemA}>PORTFOLIO</Link></li>
+            <li className={styles.navListItem}><Link href="/#portfolio" className={styles.navListItemA}>PORTFOLIO</Link></li>
             <li className={styles.navListItem}><Link href="/#about" className={styles.navListItemA}>O MNIE</Link></li>
             <li className={styles.navListItem}><Link href="/oferta" className={styles.navListItemA}>OFERTA</Link></li>
             <li className={styles.navListItem}><Link href="#contact" className={styles.navListItemA}>KONTAKT</Link></li>
@@ -38,13 +38,13 @@ const Navigation = () => {
 
             <div className='mt-16'>
               <ul className={`${styles.navListMobile}`}>
-                <li className={styles.navListItemMobile}><Link href="/portfolio" onClick={() => setOpenMenu(false)}>PORTFOLIO</Link></li>
-                <li className={styles.navListItemMobile}><Link href="/oferta" onClick={() => setOpenMenu(false)}>OFERTA</Link></li>
-                <li className={styles.navListItemMobile}><Link href="/#about" onClick={() => setOpenMenu(false)}>O MNIE</Link></li>
-                <li className={styles.navListItemMobile}><a href='https://www.facebook.com/people/Kazimierz-Skalniak-Film/100082278789126/' target='_blank' rel='noreferrer' onClick={() => setOpenMenu(false)}>FACEBOOK</a></li>
-                <li className={styles.navListItemMobile}><a href='https://www.instagram.com/kazimierz_skalniak/' target='_blank' rel='noreferrer' onClick={() => setOpenMenu(false)}>INSTAGRAM</a></li>
-                <li className={styles.navListItemMobile}><a href='https://www.youtube.com/channel/UCW0T7ISuP2Nn7BD3NHY-ihA' target='_blank' rel='noreferrer' onClick={() => setOpenMenu(false)}>YOUTUBE</a></li>
-                <li className={styles.navListItemMobile}><Link href="#contact" onClick={() => setOpenMenu(false)}>KONTAKT</Link></li>
+                <li className={styles.navListItemMobile}><Link href="/#portfolio" onClick={() => setOpenMenu(false)}>Portfolio</Link></li>
+                <li className={styles.navListItemMobile}><Link href="/oferta" onClick={() => setOpenMenu(false)}>Oferta</Link></li>
+                <li className={styles.navListItemMobile}><Link href="/#about" onClick={() => setOpenMenu(false)}>O mnie</Link></li>
+                <li className={styles.navListItemMobile}><a href='https://www.facebook.com/people/Kazimierz-Skalniak-Film/100082278789126/' target='_blank' rel='noreferrer' onClick={() => setOpenMenu(false)}>Facebook</a></li>
+                <li className={styles.navListItemMobile}><a href='https://www.instagram.com/kazimierz_skalniak/' target='_blank' rel='noreferrer' onClick={() => setOpenMenu(false)}>Instagram</a></li>
+                <li className={styles.navListItemMobile}><a href='https://www.youtube.com/channel/UCW0T7ISuP2Nn7BD3NHY-ihA' target='_blank' rel='noreferrer' onClick={() => setOpenMenu(false)}>Youtube</a></li>
+                <li className={styles.navListItemMobile}><Link href="#contact" onClick={() => setOpenMenu(false)}>Kontakt</Link></li>
               </ul>
             </div>
           </nav>
